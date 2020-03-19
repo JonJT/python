141a7cc082084b1b7e5de0e2d1c0a04cb1fae21a
JT (Jonathan Tally) <57230733+JonJT@users.noreply.github.com>
2020-03-19 13:35:57 -0700

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,8 @@
+favorite_food = input("What's my favorite food?")
+
+if favorite_food == "Cookies":
+    print("Yep, so amazing!")
+else:
+    print("Yuck! That's not it")
+
+print("Thanks for playing")
