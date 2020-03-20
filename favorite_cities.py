401af5dd7ffc86db62bb3f32b77600edbdb5bdf6
JT (Jonathan Tally) <57230733+JonJT@users.noreply.github.com>
2020-03-20 11:40:06 -0700

    Add files via upload

---
+++ favorite_cities.py
@@ -0,0 +1,7 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("Seattle, Washington")
+favorite_city("London, England")
+favorite_city("Dublin, Ireland")
